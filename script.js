851cac649de492270833898bd880f834b0c70823
Racheljjn <jian1160@mylaurier.ca>
2022-07-06 21:25:50 -0400

    code clean up

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
-let theme = localStorage.getItem("theme");
 
-if (theme == null) {
-  setTheme("light");
-} else {
-  setTheme(theme);
-}
+let toggle = document.getElementById("checkbox")
+toggle.addEventListener("click",function(){
+  let theme = localStorage.getItem("theme");
+  
+  if(theme === "dark"){
+    setTheme("light");
 
-let themeDots = document.getElementsByClassName("theme-dot");
+  }else if(theme === "light"){
+    setTheme("dark")
+  }else if(theme === null){
+    setTheme("dark")
+  }
 
-for (var i = 0; themeDots.length > i; i++) {
-  themeDots[i].addEventListener("click", function () {
-    let mode = this.dataset.mode;
-    console.log("Option clicked:", mode);
-    setTheme(mode);
-  });
-}
+  
+})
 
 function setTheme(mode) {
   if (mode == "light") {
     document.getElementById("theme-style").href = "default.css";
   }
 
-  if (mode == "blue") {
+  if (mode == "dark") {
     document.getElementById("theme-style").href = "blue.css";
-  }
-
-  if (mode == "green") {
-    document.getElementById("theme-style").href = "green.css";
-  }
-
-  if (mode == "purple") {
-    document.getElementById("theme-style").href = "purple.css";
   }
 
   localStorage.setItem("theme", mode);
